77819eb021f1bfa80f86df7830b92335e9752136
Vasanthkumar Velusamy <vasanthkumar.work@gmail.com>
2024-06-21 18:41:58 +0530

    leveraging constants for action types

--- index.js
+++ index.js
@@ -1,48 +1,4 @@
-{
-  type: 'ADD_TODO',
-  todo: {
-    id: 0,
-    name: 'Learn Redux',
-    complete: false,
-  }
-}
-
-{
-  type: 'REMOVE_TODO',
-  id: 0,
-}
-
-{
-  type: 'TOGGLE_TODO',
-  id: 0,
-}
-
-{
-  type: 'ADD_GOAL',
-  goal: {
-    id: 0,
-    name: 'Run a Marathon'
-  }
-}
-
-{
-  type: 'REMOVE_GOAL',
-  id: 0,
-}
-
-const todos = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return [...state, action.todo]
-        case 'REMOVE_TODO':
-            return state.filter((todo) => todo.id !== action.id)
-        case 'TOGGLE_TODO':
-            return state.map((todo) => todo.id !== id ? todo : {...todo, complete: !todo.complete})
-        default:
-            return state
-    }
-}
-
+// Library code
 function createStore(reducer) {
     // Store should have the following components
     // 1. the state.
@@ -77,7 +33,48 @@ function createStore(reducer) {
     }
 }
 
-const myStore = createStore(todos)
+// App code
+
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case ADD_TODO:
+            return [...state, action.todo]
+        case REMOVE_TODO:
+            return state.filter((todo) => todo.id !== action.id)
+        case TOGGLE_TODO:
+            return state.map((todo) => todo.id !== action.id ? todo : {...todo, complete: !todo.complete})
+        default:
+            return state
+    }
+}
+
+const goals = (state = [], action) => {
+    switch (action.type) {
+        case ADD_GOAL:
+            return [...state, action.goal]
+        case REMOVE_GOAL:
+            return state.filter((goal) => goal.id !== action.id)
+        default:
+            return state
+    }
+}
+
+const app = (state = {}, action) => {
+    return {
+        todos: todos(state.todos, action),
+        goals: goals(state.goals, action)
+    }
+}
+
+
+
+const myStore = createStore(app)
 
 const unsubscribe = myStore.subscribe(()=> { // adding this callback to the listeners array in the store. So it will be called when state change occurs.
     console.log('this is the new state', myStore.getState())
@@ -92,4 +89,27 @@ myStore.dispatch({
     name: 'Learn Redux',
     complete: false,
   }
+})
+
+myStore.dispatch({
+  type: 'REMOVE_TODO',
+  id: 0,
+})
+
+myStore.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 0,
+})
+
+myStore.dispatch({
+  type: 'ADD_GOAL',
+  goal: {
+    id: 0,
+    name: 'Run a Marathon'
+  }
+})
+
+myStore.dispatch({
+  type: 'REMOVE_GOAL',
+  id: 0,
 })
